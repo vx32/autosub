d0a8ce05cc6421bed8f38da4c210f89e37a16633
vx32 <x64wall@proton.me>
2026-05-11 02:11:44 +0800

    Another global initialization problem.

--- Command.cc
+++ Command.cc
@@ -1,24 +1,33 @@
 #include "Command.hh"
 
 #include <map>
+#include <memory>
 #include <sstream>
 
-std::map<std::string, Command *> g_all_commands;
+std::unique_ptr<std::map<std::string, Command *>> g_all_commands = nullptr;
+static void init_check() {
+  if (!g_all_commands) {
+    g_all_commands = std::make_unique<std::map<std::string, Command *>>();
+  }
+}
 
 bool Command::register_command(std::string name, Command *cmd) {
-  auto p = g_all_commands.insert({name, cmd});
+  init_check();
+  auto p = g_all_commands->insert({name, cmd});
   return p.second;
 }
 Command *Command::get_command(std::string name) {
-  auto it = g_all_commands.find(name);
-  if (it != g_all_commands.end()) {
+  init_check();
+  auto it = g_all_commands->find(name);
+  if (it != g_all_commands->end()) {
     return it->second;
   }
   return nullptr;
 }
 std::string Command::all_usage() {
+  init_check();
   std::stringstream iss;
-  for (auto it = g_all_commands.begin(); it != g_all_commands.end(); it++) {
+  for (auto it = g_all_commands->begin(); it != g_all_commands->end(); it++) {
     iss << it->second->usage();
   }
   return iss.str();
